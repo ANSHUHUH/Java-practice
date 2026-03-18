0db60e00ef748ddd417bced5177404b30a8a7ea5
ANSHUHUH <vishwakarmahomepc@gmail.com>
2026-03-18 18:36:44 +0530

    Create MCA class to show student name and age
    Implement MCA class with show method to display student details.

---
+++ practical 20/package course/mca.java
@@ -0,0 +1,15 @@
+package course;
+
+public class MCA {
+	public void show() {
+		int age = 20;
+		String name = "elon musk. ";
+		System.out.println("student name : "+name);
+		System.out.println("student age : "+age);
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
