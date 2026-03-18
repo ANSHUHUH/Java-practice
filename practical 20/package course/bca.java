cb5acf93acfd88db17b2b911c7ce0f306e2504bf
ANSHUHUH <vishwakarmahomepc@gmail.com>
2026-03-18 18:35:32 +0530

    Create bca.java

---
+++ practical 20/package course/bca.java
@@ -0,0 +1,15 @@
+package course;
+
+public class BCA {
+public void show() {
+	int age = 19;
+	String name = "anshu vishwakarma. ";
+	System.out.println("student name : "+name);
+	System.out.println("student age : "+age);
+}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
