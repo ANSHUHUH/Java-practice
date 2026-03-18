12bebef003f162ae040d6773ba77d4ab65cb02fd
ANSHUHUH <vishwakarmahomepc@gmail.com>
2026-03-18 18:37:40 +0530

    Create DemoCourse.java

---
+++ practical 20/package course/DemoCourse.java
@@ -0,0 +1,11 @@
+package course;
+import course.BCA;
+public class Democourse {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+BCA s1 = new BCA();
+s1.show();
+	}
+
+}
